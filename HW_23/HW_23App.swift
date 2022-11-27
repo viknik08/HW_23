f4ce89ab159504085e1490b1839b5a046895bc57
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-11-27 22:52:09 +0800

    Initial Commit

---
+++ HW_23/HW_23App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_23App.swift
+//  HW_23
+//
+//  Created by Виктор Басиев on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
